9ed181d4b81fb2a70bceca1cc2bed120cd10d45f
Bartosz Kielczewski <bartosz@kielczewski.eu>
2015-05-29 16:16:17 +0100

    Missed another Pattern that can be static

--- akanke-common/src/main/java/eu/kielczewski/akanke/common/service/document/DocumentPropertyExtractor.java
+++ akanke-common/src/main/java/eu/kielczewski/akanke/common/service/document/DocumentPropertyExtractor.java
@@ -12,12 +12,12 @@ import java.util.regex.Pattern;
 @Component
 public class DocumentPropertyExtractor {
 
-    private static final Pattern PATTERN_DOCUMENT_PROPERTY = Pattern.compile("\\s*([a-zA-Z0-9_]+)\\s*[=|:]\\s*(.+)\\s*");
+    private static final Pattern PATTERN_PROPERTY_BLOCK = Pattern.compile("[^\\s]*<!--(.*?)-->", Pattern.DOTALL);
+    private static final Pattern PATTERN_PROPERTY = Pattern.compile("\\s*([a-zA-Z0-9_]+)\\s*[=|:]\\s*(.+)\\s*");
     private static final Logger LOGGER = LoggerFactory.getLogger(DocumentPropertyExtractor.class);
 
     public Map<String, String> fromContents(String content) {
-        Pattern p = Pattern.compile("[^\\s]*<!--(.*?)-->", Pattern.DOTALL);
-        Matcher m = p.matcher(content);
+        Matcher m = PATTERN_PROPERTY_BLOCK.matcher(content);
         Map<String, String> props = new HashMap<>();
         while (m.find()) {
             String commentBlock = m.group(1);
@@ -29,7 +29,7 @@ public class DocumentPropertyExtractor {
     }
 
     private Map<String, String> extract(String src) {
-        Matcher m = PATTERN_DOCUMENT_PROPERTY.matcher(src);
+        Matcher m = PATTERN_PROPERTY.matcher(src);
         Map<String, String> props = new HashMap<>();
         while (m.find()) {
             props.put(m.group(1), m.group(2));
